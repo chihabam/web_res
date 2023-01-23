c8a136fac71f3fa740dc3ccfe713896a6d21fd09
Magus Chihaba <chihabam@gmail.com>
2023-01-22 20:22:28 -0600

    Now have the CSS running properly with the html and the flask server has a venv

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/bin/python
+from flask import Flask, render_template, url_for
+from views import views
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('resume.html')
+
+if __name__ == "__main__":
+   app.run(debug=True)
