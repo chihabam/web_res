ed84328974e1bd8f057773feb3b7383a82ab1f49
Magus Chihaba <chihabam@gmail.com>
2023-01-25 21:02:39 -0600

    removed fluff files

--- bak_app.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python
-from flask import Flask
-from views import views
-
-app = Flask(__name__)
-app.register_blueprint(views,url_prefix="/views")
-
-if __name__ =='__main__':
-   app.run(host="0.0.0.0",debug=True)
